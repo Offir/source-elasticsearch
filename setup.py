7abbbaf295ac74264005fd0e626c9287e588339e
Lior Rozen <lixroz+github@gmail.com>
2017-01-15 11:31:55 +0200

    Updated elasticsearch sdk version to 2.4.1

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name="panoply_elasticsearch",
-    version="1.0.1",
+    version="1.0.2",
     description="Panoply Data Source for Elasticsearch",
     author="Lior Rozen",
     author_email="lior@panoply.io",
     url="http://panoply.io",
     install_requires=[
         "panoply-python-sdk==1.3.2",
-        "elasticsearch>=1.0.0,<2.0.0",
+        "elasticsearch==2.4.1",
     ],
 
     # place this package within the panoply package namespace
