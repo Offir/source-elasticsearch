9dbf463487ae18eb8060abdde70867cfd82311e9
hadasi <hadas@panoply.io>
2017-09-14 10:39:54 +0300

    updated version to 1.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="panoply_elasticsearch",
-    version="1.0.3",
+    version="1.0.4",
     description="Panoply Data Source for Elasticsearch",
     author="Lior Rozen",
     author_email="lior@panoply.io",
